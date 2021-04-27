24a212ff6ff5638e1db9d028670587c3a8052a8f
zsp <153962226187@163.com>
2021-04-27 11:55:20 +0800

    activiti流创建完成，晚上创造activiti流程

---
+++ springboot/zspoa-activiti/src/main/java/com/zsp/zspoaactiviti/ZspoaActivitiApplication.java
@@ -0,0 +1,13 @@
+package com.zsp.zspoaactiviti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZspoaActivitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZspoaActivitiApplication.class, args);
+	}
+
+}
---
+++ springboot/zspoa-activiti/src/test/java/com/zsp/zspoaactiviti/ZspoaActivitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsp.zspoaactiviti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZspoaActivitiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
