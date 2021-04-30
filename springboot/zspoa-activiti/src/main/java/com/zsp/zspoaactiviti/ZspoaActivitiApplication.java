16aa8661d20f484617ca43cd91e221b9e6a1d1ce
zsp <15396226187@163.com>
2021-05-01 02:48:35 +0800

    部署到了台式上，activiti生成流程出现问题

--- springboot/zspoa-activiti/src/main/java/com/zsp/zspoaactiviti/ZspoaActivitiApplication.java
+++ springboot/zspoa-activiti/src/main/java/com/zsp/zspoaactiviti/ZspoaActivitiApplication.java
@@ -2,8 +2,9 @@ package com.zsp.zspoaactiviti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZspoaActivitiApplication {
 
