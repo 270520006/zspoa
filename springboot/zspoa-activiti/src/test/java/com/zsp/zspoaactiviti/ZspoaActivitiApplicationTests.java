fce7b29bcb7edc2002dfaab448b8bb799c0fb7f6
zsp <15396226187@163.com>
2021-05-01 20:53:46 +0800

    充分熟悉了activiti的api，着手开发activiti模块

--- springboot/zspoa-activiti/src/test/java/com/zsp/zspoaactiviti/ZspoaActivitiApplicationTests.java
+++ springboot/zspoa-activiti/src/test/java/com/zsp/zspoaactiviti/ZspoaActivitiApplicationTests.java
@@ -2,10 +2,13 @@ package com.zsp.zspoaactiviti;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.task.Task;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ZspoaActivitiApplicationTests {
@@ -30,10 +33,72 @@ class ZspoaActivitiApplicationTests {
 		processEngine.getRepositoryService()//得到库服务
 				.createDeployment()//创建部署
 				.name("askRest")
-				.addClasspathResource("bmp/rest.bpmn20.xml") //导入流程图  !!!!一定要加bpmn20
-				.addClasspathResource("bmp/rest.bpmn20.png")   //导入流程文件!!!!一定要加bpmn20
+				.category("流程发起人")
+				.addClasspathResource("bpm/rest.bpmn20.xml") //导入流程图  !!!!一定要加bpmn20
+				.addClasspathResource("bpm/rest.bpmn20.png")   //导入流程文件!!!!一定要加bpmn20
 				.deploy();   //开始部署
 	}
+	//2、启用创建的流程图进行审批
+	@Test
+	public void testStartProcessInstance(){
+		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+		//act_re_procdef表里的id，如果没生成，去看上一步，真是哔了狗
+		processEngine.getRuntimeService()
+				.startProcessInstanceById("myProcess_1:2:7504");
 
+	}
+
+	//3、请假人发出请假申请
+	@Test
+	public void testAsk(){
+//        获取activity7的引擎
+		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//    查看act_ru_task表，然后把id给上，发起请假申请
+		processEngine.getTaskService()
+				.complete("2505");
+
+	}
+	//    4、老师查看请假要求
+	@Test
+	public void queryTask(){
+//          获得流引擎
+		ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+//    创造搜索，下面的teacher是我们在创建bpmn的时候写的assignee值
+		List<Task> student = engine.getTaskService().createTaskQuery()
+//				.taskAssignee("student")  //根据审批人查询
+				.taskOwner("zsp")  //根据任务拥有者进行查询
+				.list();
+		for (Task task : student) {
+			System.out.println(task);
+		}
+	}
+
+	//5、班主任审批
+	@Test
+	public void testFinishTask_manager(){
+//        获取流引擎
+		ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+//    这里的id是上面查询生成的请假申请id
+		engine.getTaskService()
+				.complete("15002");
+
+
+	}
+	@Test
+	public void testSetOwner(){
+		ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+		List<Task> teacher = engine.getTaskService()
+				.createTaskQuery()
+				.taskAssignee("student")
+				.list();
+		for (Task task : teacher) {
+			System.out.println(task);
+			System.out.println(task.getProcessInstanceId());
+		}
+		String id = teacher.get(0).getId();
+		engine.getTaskService().complete(id);
+
+
+	}
 
 }
