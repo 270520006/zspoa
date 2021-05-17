bdaa7f6c7b98460063e58ba5d8a9e1c5d094fe84
zsp <15396226187@163.com>
2021-05-18 02:49:56 +0800

    创建了elasticsearch的docker容器，准备开始做搜索功能

---
+++ springboot/zspoa-auth-server/src/main/java/com/zsp/zspoaauthserver/ZspoaAuthServerApplication.java
@@ -0,0 +1,17 @@
+package com.zsp.zspoaauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZspoaAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZspoaAuthServerApplication.class, args);
+    }
+
+}
---
+++ springboot/zspoa-auth-server/src/test/java/com/zsp/zspoaauthserver/ZspoaAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsp.zspoaauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZspoaAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
