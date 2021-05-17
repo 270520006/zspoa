8a6e144e3b6f5875861536d2838c81919493efa6
zsp <15396226187@163.com>
2021-05-17 23:38:56 +0800

    将所有模块使用gateway转发到子域名上访问

---
+++ springboot/zspoa-activiti/src/main/java/com/zsp/zspoaactiviti/config/SpringSessionConfig.java
@@ -0,0 +1,20 @@
+package com.zsp.zspoaactiviti.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class SpringSessionConfig {
+	
+	/**
+     * 更换序列化器
+     * @return
+     */
+    @Bean("springSessionDefaultRedisSerializer")
+    public RedisSerializer setSerializer(){
+        return new GenericJackson2JsonRedisSerializer();
+    }
+    
+}
