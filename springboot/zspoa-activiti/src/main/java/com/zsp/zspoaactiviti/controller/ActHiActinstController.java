6a9e94cb78923c3969ec50bc9ed0c3c08fc138f0
zsp <15396226187@163.com>
2021-06-08 23:23:32 +0800

    编写完毕流程管理模块，正在研究在线制作流程图

--- springboot/zspoa-activiti/src/main/java/com/zsp/zspoaactiviti/controller/ActHiActinstController.java
+++ springboot/zspoa-activiti/src/main/java/com/zsp/zspoaactiviti/controller/ActHiActinstController.java
@@ -1,9 +1,23 @@
 package com.zsp.zspoaactiviti.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.zsp.utils.R;
+import com.zsp.zspoaactiviti.entity.ActHiActinst;
+import com.zsp.zspoaactiviti.service.ActHiActinstService;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -16,6 +30,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("acti/actinst")
 public class ActHiActinstController {
+    @Autowired
+    ActHiActinstService actHiActinstService;
+    @PostMapping("/queryActinst")
+    public R createDeployment(@RequestParam("proID") String proID, HttpSession session){
 
+        System.out.println(session.getAttribute("user"));
+        try {
+            QueryWrapper<ActHiActinst> queryWrapper = new QueryWrapper<>();
+            queryWrapper.eq("PROC_INST_ID_",proID);
+            List<ActHiActinst> list = actHiActinstService.list(queryWrapper);
+            List<ActHiActinst> actHiActinstList = list.stream()
+                    .sorted(Comparator.comparing(ActHiActinst::getStartTime))
+                    .collect(Collectors.toList());
+            System.out.println(actHiActinstList);
+            return R.ok().put("createDeploymentMsg",actHiActinstList);
+
+        } catch (Exception e) {
+            return R.error().put("createDeploymentMsg","查询流程失败！");
+        }
+
+    }
 }
 
